d25e6714b5a4c9b203f0af93ed3c543d404f1e54
ziyanul <ziyanul@gmail.com>
2026-09-12 09:33:22 +0700

    fix

--- application/views/pvdc/tambah.php
+++ application/views/pvdc/tambah.php
@@ -3,24 +3,24 @@
     <h1 class="h3 mb-2 text-gray-800">Input Data Pemakaian PVDC & WIRE</h1>
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="<?= base_url('area') ?>"><i class="fas fa-arrow-left"></i> Fokus
-                    Area</a></li>
+            <li class="breadcrumb-item"><a href="<?= base_url('pvdc') ?>"><i class="fas fa-arrow-left"></i> Fokus
+                    PVDC & WIRE</a></li>
             <li class="breadcrumb-item active" aria-current="page">Tambah</li>
         </ol>
     </nav>
     <?php if ($this->session->flashdata('success_msg')): ?>
-    <div class="alert alert-success text-center">
-        <i class="fas fa-check"></i>
-        <?= $this->session->flashdata('success_msg') ?>
-    </div>
-    <br>
+        <div class="alert alert-success text-center">
+            <i class="fas fa-check"></i>
+            <?= $this->session->flashdata('success_msg') ?>
+        </div>
+        <br>
     <?php endif ?>
     <?php if ($this->session->flashdata('error_msg')): ?>
-    <div class="alert alert-danger  text-center">
-        <i class="fas fa-times"></i>
-        <?= $this->session->flashdata('error_msg') ?>
-    </div>
-    <br>
+        <div class="alert alert-danger  text-center">
+            <i class="fas fa-times"></i>
+            <?= $this->session->flashdata('error_msg') ?>
+        </div>
+        <br>
     <?php endif ?>
     <div class="card shadow mb-4">
         <div class="card-body">
@@ -30,9 +30,9 @@
                         <label class="form-label">Tanggal Produksi : </label>
                         <select name="planning_uuid" id="planning_uuid" class="form-control" required>
                             <?php foreach ($data as $row) : ?>
-                            <option value="<?= $row->uuid_planning ?>">
-                                <?= $row->tanggal ?> - <?= $row->nama_varian ?>
-                            </option>
+                                <option value="<?= $row->uuid_planning ?>">
+                                    <?= $row->tanggal ?> - <?= $row->nama_varian ?>
+                                </option>
                             <?php endforeach; ?>
                         </select>
                     </div>
